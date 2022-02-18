cc971ee9d41211317a3258fa753dab8aefd4eff9
Coolicc <o.g.mank@gmail.com>
2022-02-18 09:24:54 -0600

    CORS

--- src/config/createApp.ts
+++ src/config/createApp.ts
@@ -11,7 +11,8 @@ const createApp = () => {
       // origin: process.env.CLIENT || 'http://localhost:3000',
       // origin: /^.+maev\.me$/,
       // origin: 'http://localhost:3000',
-      origin: ['https://maev.me', /^.+maev\.me$/, 'https://api.maev.me', 'https://www.maev.me/'],
+      // origin: ['https://maev.me', /^.+maev\.me$/, 'https://api.maev.me', 'https://www.maev.me/'],
+      origin: 'https://maev.me',
       credentials: true,
     })
   )
