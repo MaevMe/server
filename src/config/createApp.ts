3d78a15f0a95ce6dcf524397057d5cda7e08f023
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2022-02-19 08:54:03 +0100

    CORS

--- src/config/createApp.ts
+++ src/config/createApp.ts
@@ -14,6 +14,24 @@ const createApp = () => {
     })
   )
 
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+    // Pass to next layer of middleware
+    next()
+  })
+
   if (!process.env.SECRET) {
     throw new Error('Missing SECRET variable in your .env, which is needed for express-sessions')
   }
