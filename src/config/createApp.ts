3178ef19cfb8f551d0956cfc905334807e813244
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2022-02-22 08:07:05 +0100

    Cookies

--- src/config/createApp.ts
+++ src/config/createApp.ts
@@ -25,6 +25,7 @@ const createApp = () => {
         sameSite: process.env.ENV === 'PROD' ? 'strict' : 'lax',
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 6.75,
+        domain: 'maev.me',
       },
     })
   )
