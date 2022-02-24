11d7604a988e56923fb2bb165c87bfd95706f938
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2022-02-24 23:13:09 +0100

    Mongo Session Store

--- src/config/connectMongo.ts
+++ src/config/connectMongo.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 
 const connectMongo = async () => {
   try {
+    if (!process.env.MONGO) throw new Error('🚨 Missing Mongo in .env')
+
     await mongoose.connect(process.env.MONGO)
     console.log('🙊 MongoDB Connectd')
   } catch (error) {
