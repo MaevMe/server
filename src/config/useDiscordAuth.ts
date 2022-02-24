b1ddf79b6d171f1e54c4b75a55c167058633a049
Coolicc <o.g.mank@gmail.com>
2022-02-24 14:23:38 -0600

    Init

--- src/config/useDiscordAuth.ts
+++ src/config/useDiscordAuth.ts
@@ -10,7 +10,7 @@ const useDiscordAuth = (app: Express) => {
   app.get('/forward', (req, res) => {
     const query = [
       `client_id=${CLIENT_ID}`,
-      `redirect_uri=${encodeURIComponent('https://www.maev.me' + '/callback')}`,
+      `redirect_uri=${encodeURIComponent('https://www.base-client.vercel.app' + '/callback')}`,
       'response_type=code',
       `scope=${scope}`,
     ].join('&')
@@ -26,7 +26,7 @@ const useDiscordAuth = (app: Express) => {
       const params = new URLSearchParams({
         client_id: CLIENT_ID as string,
         client_secret: CLIENT_SECRET as string,
-        redirect_uri: 'https://www.maev.me/callback',
+        redirect_uri: 'https://www.base-client.vercel.app/callback',
         code: code as string,
         grant_type: 'authorization_code',
         scope,
