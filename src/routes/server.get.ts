4c23a6f83dd4c07d2e91b7072f901500bc8d61b5
Coolicc <o.g.mank@gmail.com>
2022-02-25 16:22:54 -0600

    Server get/post

--- src/routes/server.get.ts
+++ src/routes/server.get.ts
@@ -4,9 +4,11 @@ import Server from '../models/Server'
 export default new Route(
   async (req, res) => {
     const { serverID } = req.params
+    console.log('serverID: ', serverID)
 
     try {
       const server = await Server.findOne({ id: serverID })
+      console.log('server: ', server)
       res.send(server)
     } catch (err) {
       res.status(500).send({ err })
