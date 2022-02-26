855a507efd5526766681deeb304a90d98d52ad37
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2022-02-26 12:47:22 +0100

    Get Server Cleanup and Minor Fixes

--- src/routes/server.get.ts
+++ src/routes/server.get.ts
@@ -1,6 +1,11 @@
 import Route from '../structure/Route'
 import Server from '../models/Server'
 import discord from '../utils/discord'
+import { RESTGetAPIGuildChannelsResult, APIChannel } from 'discord-api-types/v9'
+
+const map = (channel: APIChannel) => {
+  return { id: channel.id, name: channel.name }
+}
 
 export default new Route(
   async (req, res) => {
@@ -10,29 +15,17 @@ export default new Route(
 
     try {
       const server = await Server.findOne({ id: serverID })
-      const channels = (await discord.api.get(`/guilds/${serverID}/channels`, { headers })).data
+      const channels: RESTGetAPIGuildChannelsResult = (
+        await discord.api.get(`/guilds/${serverID}/channels`, { headers })
+      ).data
 
       if (!server) return res.status(404).send({ err: 'No server' })
-      if (!channels) {
-        console.log('@server.get, no channels', channels)
-        return res.status(505).send({ err: 'No channels' })
-      }
+      if (!channels) return res.status(404).send({ err: 'No channels' })
 
-      console.log('@server.get, with channels', channels)
+      const voiceChannels = channels.filter(channel => channel.type === 2).map(map)
+      const categories = channels.filter(channel => channel.type === 4).map(map)
 
-      server.voiceChannels = channels
-        .filter((channel: any) => channel.type === 2)
-        .map((channel: any) => {
-          channel.id, channel.name
-        })
-
-      server.categories = channels
-        .filter((channel: any) => channel.type === 4)
-        .map((channel: any) => {
-          channel.id, channel.name
-        })
-
-      return res.status(200).send(server)
+      return res.status(200).send({ ...server, categories, voiceChannels })
     } catch (err) {
       console.error('@server.get', err)
       return res.status(500).send({ err })
