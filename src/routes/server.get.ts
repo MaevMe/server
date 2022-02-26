966c116020306582bbc33042b1f04610b5dcc92c
Coolicc <o.g.mank@gmail.com>
2022-02-25 18:17:00 -0600

    Server get/post

--- src/routes/server.get.ts
+++ src/routes/server.get.ts
@@ -1,14 +1,35 @@
 import Route from '../structure/Route'
 import Server from '../models/Server'
+import axios from 'axios'
 
 export default new Route(
   async (req, res) => {
     const { serverID } = req.params
     console.log('serverID: ', serverID)
 
+    const { tokenType, accessToken } = req.session
+
+    const headers = {
+      authorization: `${tokenType} ${accessToken}`,
+    }
+
     try {
       const server = await Server.findOne({ id: serverID })
+      const channels = (
+        await axios.get(`https://discord.com/api/guilds/${serverID}/channels`, { headers })
+      ).data
+      server.voiceChannels = channels
+        .filter((channel: any) => channel.type === 2)
+        .map((channel: any) => {
+          channel.id, channel.name
+        })
+      server.categories = channels
+        .filter((channel: any) => channel.type === 2)
+        .map((channel: any) => {
+          channel.id, channel.name
+        })
       console.log('server: ', server)
+
       res.send(server)
     } catch (err) {
       res.status(500).send({ err })
--- src/routes/server.post.ts
+++ src/routes/server.post.ts
@@ -3,8 +3,10 @@ import Server from '../models/Server'
 
 export default new Route(
   async (req, res) => {
-    const { server } = req.body
+    const { serverData } = req.body
     const { mongoServerID } = req.params
+
+    const { voiceChannels, categories, ...server } = serverData
 
     try {
       const updatedServer = await Server.findByIdAndUpdate(mongoServerID, server)
